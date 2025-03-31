51be497daaf1592c357cbec8aaa43a86b361fe8b
Martin Fuentes <martintati13@gmail.com>
2025-03-31 09:48:11 -0300

    conexion con base de datos

---
+++ src/templates/database.php
@@ -0,0 +1,21 @@
+<?php
+$config = require __DIR__ ."'/database_config.php";
+
+$dsn = "mysql:host={$config['host']};dbname={config['dbname']};charset=utf8"; #DSN = DATA SOURCE NAME host, dbname, charset; Son los parametros
+$username = $config['username']; #su nombre de usuario, por defecto en XAMPP es root
+$password = $config['password']; #la contraseña, por defecto en XAMPP es '' (osea vacia)
+
+
+try { #creamos una instancia de PDO (una conexion a una base de datos, le pasamos los parametros) y con [] configuramos
+    $pdo = new PDO($dsn, $username, $password, [ 
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, #esto hace que cualquier error frene el programa y sea visible su descripcion
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, #los resultados de una consulta se devuelven en un array asociativo
+        PDO::ATTR_EMULATE_PREPARES => false #asegura que las consultas provengan de MySQL, evita inyecciones SQL (hackeos) 
+]);
+    #si todo sale bien se conecta
+    echo "conexion exitosa";
+}
+    #si todo sale mal, explota
+catch (PDOException $e){
+    die("error en la conexion: ". $e->getMessage());
+}
